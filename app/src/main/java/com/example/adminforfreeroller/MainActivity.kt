4f3387f4e134416aa2df90df999456e00824fc91
Victor Spirin <greatgomer2@gmail.com>
2020-06-03 15:32:07 +0300

    Check is ready

--- app/src/main/java/com/example/adminforfreeroller/MainActivity.kt
+++ app/src/main/java/com/example/adminforfreeroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.adminforfreeroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,16 +20,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sentData = findViewById<Button>(R.id.buttonSetData)
+        val editLat = findViewById<EditText>(R.id.editText_lat)
+        val editLon = findViewById<EditText>(R.id.editText_lon)
+        val editName = findViewById<EditText>(R.id.editText_name)
+        val editOpt = findViewById<EditText>(R.id.editText_opt)
         database = FirebaseDatabase.getInstance().getReference(places_name)
-    }
 
-    fun saveData(view: View) {
-        var id = database.getKey().toString()
-        var lat = editText_lat.toString()
-        var lon = editText_lon.toString()
-        var name = editText_name.toString()
-        var opt = editText_opt.toString()
-        val addData = AddData(id, lat, lon, name, opt)
-        database.push().setValue(addData)
+        sentData.setOnClickListener {
+            var id = database.getKey().toString()
+            var lat = editLat.text.toString()
+            var lon = editLon.text.toString()
+            var name = editName.text.toString()
+            var opt = editOpt.text.toString()
+            if(!lat.isEmpty() && !lon.isEmpty() && !name.isEmpty() && !opt.isEmpty()){
+                val addData = AddData(id, lat, lon, name, opt)
+                database.push().setValue(addData)
+            }else{
+                Toast.makeText(this, R.string.check_add, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
