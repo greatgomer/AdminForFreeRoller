b758383f877ac49385d4772751ee4ea1a83443a1
Victor Spirin <greatgomer2@gmail.com>
2020-06-10 15:57:51 +0300

    Redesign

--- app/src/main/java/com/svdgroup/adminforinline/MainActivity.kt
+++ app/src/main/java/com/svdgroup/adminforinline/MainActivity.kt
@@ -12,59 +12,68 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var database: DatabaseReference
-    private var places_name = "Places"
-    var city = ""
+//    private lateinit var database: DatabaseReference
+//    private var places_name = "Places"
+//    var city = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var spinner: Spinner? = null
-        val sentData = findViewById<Button>(R.id.buttonSetData)
-        val editLat = findViewById<EditText>(R.id.editText_lat)
-        val editLon = findViewById<EditText>(R.id.editText_lon)
-        val editName = findViewById<EditText>(R.id.editText_name)
-        val editOpt = findViewById<EditText>(R.id.editText_opt)
-        val cities = resources.getStringArray(R.array.cities)
+//        var spinner: Spinner? = null
+//        val sentData = findViewById<Button>(R.id.buttonSetData)
+//        val editLat = findViewById<EditText>(R.id.editText_lat)
+//        val editLon = findViewById<EditText>(R.id.editText_lon)
+//        val editName = findViewById<EditText>(R.id.editText_name)
+//        val editOpt = findViewById<EditText>(R.id.editText_opt)
+//        val cities = resources.getStringArray(R.array.cities)
+//
+//        spinner = this.spinner_city
+//
+//        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, cities)
+//        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        spinner!!.adapter = aa
 
-        spinner = this.spinner_city
 
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, cities)
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner!!.adapter = aa
+//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                city = cities[position]
+//            }
+//        }
 
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                city = cities[position]
-            }
-        }
-
-        database = FirebaseDatabase.getInstance().getReference(places_name)
-
-        sentData.setOnClickListener {
-            val id = database.key.toString()
-            val lat = editLat.text.toString()
-            val lon = editLon.text.toString()
-            val name = editName.text.toString()
-            val opt = editOpt.text.toString()
-            database = FirebaseDatabase.getInstance().getReference(places_name).child(city)
-            if(lat.isNotEmpty() && lon.isNotEmpty() && name.isNotEmpty() && opt.isNotEmpty() && city.isNotEmpty()){
-                val addData = AddData(id, lat, lon, name, opt)
-                database.push().setValue(addData)
-                Toast.makeText(this, R.string.check_is_successfully, Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, R.string.check_add, Toast.LENGTH_SHORT).show()
-            }
-        }
+//        database = FirebaseDatabase.getInstance().getReference(places_name)
+//
+//        sentData.setOnClickListener {
+//            val id = database.key.toString()
+//            val lat = editLat.text.toString()
+//            val lon = editLon.text.toString()
+//            val name = editName.text.toString()
+//            val opt = editOpt.text.toString()
+//            database = FirebaseDatabase.getInstance().getReference(places_name).child(city)
+//            if(lat.isNotEmpty() && lon.isNotEmpty() && name.isNotEmpty() && opt.isNotEmpty() && city.isNotEmpty()){
+//                val addData = AddData(id, lat, lon, name, opt)
+//                database.push().setValue(addData)
+//                Toast.makeText(this, R.string.check_is_successfully, Toast.LENGTH_SHORT).show()
+//            }else{
+//                Toast.makeText(this, R.string.check_add, Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
-    fun onButtonClick(view: View) {
+    fun add_marker(view: View) {
+        val intent = Intent(this, MarkersActivity::class.java)
+        startActivity(intent)
+    }
+    fun add_train(view: View) {
         val intent = Intent(this, TrainsActivity::class.java)
         startActivity(intent)
     }
+
+//    fun onButtonClick(view: View) {
+//        val intent = Intent(this, TrainsActivity::class.java)
+//        startActivity(intent)
+//    }
 }
--- app/src/main/java/com/svdgroup/adminforinline/MarkersActivity.kt
+++ app/src/main/java/com/svdgroup/adminforinline/MarkersActivity.kt
@@ -8,32 +8,31 @@ import android.view.View
 import android.widget.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_add_trains.*
+import kotlinx.android.synthetic.main.activity_markers.*
 
-class TrainsActivity : AppCompatActivity() {
+class MarkersActivity : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
-    var style_array = arrayOf("SLALOM", "SLIDE", "JUMP", "AGGRESSIV")
-    private var trains_name = "Trains"
-    var style = ""
+    private var places_name = "Places"
+    var city = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_trains)
-
-        var spinner:Spinner? = null
-        val editName = findViewById<EditText>(R.id.editText_trains_name)
-        val editPicture = findViewById<EditText>(R.id.editText_picture_link)
-        val editComplexity = findViewById<EditText>(R.id.editText_complexity)
-        val editDescription = findViewById<EditText>(R.id.editText_train_description)
-        val editVideo = findViewById<EditText>(R.id.editText_video_link)
-        val sentTrain = findViewById<Button>(R.id.button_add_train)
+        setContentView(R.layout.activity_markers)
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        spinner = this.spinner_style
+        var spinner: Spinner? = null
+        val sentData = findViewById<Button>(R.id.buttonSetData)
+        val editLat = findViewById<EditText>(R.id.editText_lat)
+        val editLon = findViewById<EditText>(R.id.editText_lon)
+        val editName = findViewById<EditText>(R.id.editText_name)
+        val editOpt = findViewById<EditText>(R.id.editText_opt)
+        val cities = resources.getStringArray(R.array.cities)
 
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, style_array)
+        spinner = this.spinner_city
+
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, cities)
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner!!.adapter = aa
 
@@ -43,29 +42,27 @@ class TrainsActivity : AppCompatActivity() {
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                style = style_array[position]
+                city = cities[position]
             }
         }
 
-        database = FirebaseDatabase.getInstance().getReference(trains_name)
+        database = FirebaseDatabase.getInstance().getReference(places_name)
 
-        sentTrain.setOnClickListener {
+        sentData.setOnClickListener {
             val id = database.key.toString()
+            val lat = editLat.text.toString()
+            val lon = editLon.text.toString()
             val name = editName.text.toString()
-            val picture = editPicture.text.toString()
-            val complexity = editComplexity.text.toString()
-            val description = editDescription.text.toString()
-            val video = editVideo.text.toString()
-            database = FirebaseDatabase.getInstance().getReference(trains_name).child(style)
-            if(style.isNotEmpty() && name.isNotEmpty() && picture.isNotEmpty() && complexity.isNotEmpty() && description.isNotEmpty() && video.isNotEmpty()){
-                val addTrain = AddTrains(id, name, picture, complexity, description, video)
-                database.push().setValue(addTrain)
+            val opt = editOpt.text.toString()
+            database = FirebaseDatabase.getInstance().getReference(places_name).child(city)
+            if(lat.isNotEmpty() && lon.isNotEmpty() && name.isNotEmpty() && opt.isNotEmpty() && city.isNotEmpty()){
+                val addData = AddData(id, lat, lon, name, opt)
+                database.push().setValue(addData)
                 Toast.makeText(this, R.string.check_is_successfully, Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText(this, R.string.check_add, Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
